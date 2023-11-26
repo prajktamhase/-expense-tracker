22f67572f7b36c8929852cb6fc5ddb7e3155c2d5
prajktamhase <mhaseprajkta@gmail.com>
2023-11-26 21:36:04 +0530

    server completed

--- server/index.js
+++ server/index.js
@@ -1,26 +1,44 @@
 import mongoose from "mongoose";
-import express  from "express";
+import express from "express";
 import dotenv from 'dotenv';
+import getApiTransaction from "./../server/util.js"
+import Transaction from "./models/Transaction.js";
 dotenv.config();
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 
-const connectDB=async()=>{
-const connection= await mongoose.connect(process.env.MONGO_URI);
+const connectDB = async () => {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
 
-if(connection){
-    console.log('MongoDB Connected....💖')
-}
+    if (connection) {
+        console.log('MongoDB Connected....💖')
+    }
 }
 connectDB();
 
+app.get('/api/health', async (req, res) => {
+    res.json({
+        success: true,
+        message: "server is rinning"
+    })
+})
 
+app.post('/api/transaction', getApiTransaction)
 
+app.get('/api/transactions', async (req, res) => {
+    const allTransaction = await Transaction.find();
 
-const PORT= process.env.PORT || 5000;
+    res.json({
+        success: true,
+        message: "Successfully fetched all transaction",
+        data: allTransaction,
+    })
+})
 
-app.listen(PORT,()=>{
-console.log(`Server is running on ${PORT}`)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
 })
